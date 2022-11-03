a97f568707d118eea1252c093e49ec1e05b526e5
jonasbarth <jb901@exeter.ac.uk>
2022-11-03 19:45:00 +0100

    Parsing * add logic for parsing descriptions, tags, nearby places, address, coordinates * added tests * renamed alt into lat for latitude * made lat and long floats instead of ints

--- parse/parser.py
+++ parse/parser.py
@@ -1,6 +1,8 @@
 """A module that contains logic for parsing HTML pages from Atlas Obscura"""
 from bs4 import BeautifulSoup
 
+import unicodedata
+
 class PlaceParser:
     """Parses a HTML file of a place in Atlas Obscura."""
     html: str
@@ -12,8 +14,8 @@ class PlaceParser:
     short_desc: str
     nearby: []
     address: str
-    alt: int
-    long: int
+    lat: float
+    long: float
     editors: []
     publication_date: any
     related: []
@@ -35,8 +37,22 @@ class PlaceParser:
         self.name = parser.find("h1", class_="DDPage__header-title").text
         self.num_people_visited = int(parser.find_all("div", class_="title-md item-action-count")[0].text) # should use more precise class
         self.num_people_want = int(parser.find_all("div", class_="title-md item-action-count")[1].text)
+        self.short_desc = parser.find("h3", class_="DDPage__header-dek").text.strip()
+        self.place_tags = list(map(lambda anchor : anchor.text.strip(), parser.find_all("a", class_="itemTags__link")))
 
+        # The description is in multiple <p> elements. We need to find them all, strip whitespace characters and join them back
+        desc_parts = list(map(lambda p : p.text.strip(), parser.find("div", {"id": "place-body"}).find_all("p")))
+        desc_parts = list(filter(lambda desc : bool(desc), desc_parts))
+        self.desc = ' '.join(desc_parts)
 
+        self.nearby = list(map(lambda near : near.text.strip(), parser.find_all('div', class_='DDPageSiderailRecirc__item-title')))
+
+        # The address is written across multiple lines, so we need to remove the white space and join them to have a single line.
+        address_parts = list(filter(lambda address_part : bool(address_part.text.strip()), parser.find('address').find("div").contents))
+        self.address = ', '.join(map(lambda address_part : address_part.text.strip(), address_parts))
+
+        coordinates = parser.find('div', class_='DDPageSiderail__coordinates').text.strip().split(sep=',')
+        self.lat, self.long = tuple(map(float, coordinates))
 
 
 """
