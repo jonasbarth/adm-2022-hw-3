212fd91d51c80126dd88a9ba02b379dce8e211eb
jonasbarth <jb901@exeter.ac.uk>
2022-11-05 00:08:46 +0100

    Index * create reader for reading html files to string * change reading place to also return names

--- reader/__init__.py
+++ reader/__init__.py
@@ -1,1 +1,2 @@
 from .tsv_reader import read_place_desc
+from .html_reader import read_html_from
--- reader/tsv_reader.py
+++ reader/tsv_reader.py
@@ -6,5 +6,5 @@ import pandas as pd
 def read_place_desc(path: str):
     """Reads all the place description stored in a tsv file from the given path and returns them."""
 
-    place = pd.read_csv(path, sep='\t', usecols=['desc'], encoding='utf-8')
-    return place.desc
+    place = pd.read_csv(path, sep='\t', usecols=['name', 'desc'], encoding='utf-8')
+    return place.name, place.desc
