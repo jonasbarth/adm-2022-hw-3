a8bae9b415aae5563796fc77aa5c1e89ef134aab
jonasbarth <jb901@exeter.ac.uk>
2022-11-04 11:11:37 +0100

    Index * change writer to write multiple places into a file * adapt test

--- tests/test_tsv_writer.py
+++ tests/test_tsv_writer.py
@@ -25,5 +25,5 @@ def assert_no_file():
 
 
 def test_that_file_is_created(place):
-    tsv_path = write_place_to_tsv('./', place)
-    os.remove(tsv_path)
+    tsv_path = write_place_to_tsv('./', [place, place])
+    #os.remove(tsv_path)
