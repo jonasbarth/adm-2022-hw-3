dc34bb06def6cc8a11939fb26a6d2f8c65d210d2
jonasbarth <jb901@exeter.ac.uk>
2022-11-05 00:47:50 +0100

    Index * add TODOs * create empty query method * implement save and load methods for index * add small class docstring

--- index/conjunctive_index.py
+++ index/conjunctive_index.py
@@ -1,19 +1,34 @@
 """Module for a conjunctive index"""
+import os
+import json
 
-
+#TODO create Index super class
 class ConjunctiveIndex:
-
-    # how to build?
-    # add word, document
+    """Class for building a conjunctive index."""
     def __init__(self):
         self.index = {}
 
+    #TODO make the path check into a decorator
     def load(self, path):
         """Loads a stored conjunctive index."""
-        pass
+        if not os.path.isdir(path):
+            raise OSError(f'The path: {path} does not exist.')
+
+        file_name = 'conjunctive_index.json'
+        full_path = f'{path}/{file_name}'
+
+        with open(full_path, 'r') as file:
+            self.index = json.load(file)
 
     def save(self, path):
         """Saves the conjunctive index into a file."""
+        if not os.path.isdir(path):
+            raise OSError(f'The path: {path} does not exist.')
+
+        file_name = 'conjunctive_index.json'
+        full_path = f'{path}/{file_name}'
+        with open(full_path, 'w') as file:
+            json.dump(self.index, file)
 
     def put(self, word_id: int, document: str):
         try:
@@ -28,3 +43,6 @@ class ConjunctiveIndex:
 
         except KeyError:
             return []
+
+    def query(self, query_words):
+        pass
