326b68dadedef1acc24c0ba19714f97b9d71ea5f
jonasbarth <jb901@exeter.ac.uk>
2022-11-04 10:09:55 +0100

    Parser * add method for writing to tsv * add tests

---
+++ tests/test_tsv_writer.py
@@ -0,0 +1,29 @@
+"""Unit tests for tsv writer"""
+import codecs
+import os
+
+import pytest
+
+from parse import PlaceParser
+from writer import write_place_to_tsv
+
+
+@pytest.fixture
+def place():
+    with codecs.open("./resources/City Hall Station – New York, New York - Atlas Obscura.html", "r", "utf-8") as file:
+        html = file.read()
+        parser = PlaceParser(html)
+        return parser.parse()
+
+
+@pytest.fixture(autouse=True)
+def assert_no_file():
+    path = './city_hall_station.tsv'
+    assert not os.path.exists(path)
+    yield
+    assert not os.path.exists(path)
+
+
+def test_that_file_is_written(place):
+    tsv_path = write_place_to_tsv('./', place)
+    os.remove(tsv_path)
