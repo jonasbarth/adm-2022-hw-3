2fb35e10b2130102024f03ec77584ce5fd1b14bb
jonasbarth <jb901@exeter.ac.uk>
2022-11-05 22:51:08 +0100

    Index * format code * add docstrings to index class * use a priority queue for store the documents

--- index/conjunctive_index.py
+++ index/conjunctive_index.py
@@ -1,14 +1,17 @@
 """Module for a conjunctive index"""
 import os
 import json
+from queue import PriorityQueue
 
-#TODO create Index super class
+
+# TODO create Index super class
 class ConjunctiveIndex:
     """Class for building a conjunctive index."""
+
     def __init__(self):
         self.index = {}
 
-    #TODO make the path check into a decorator
+    # TODO make the path check into a decorator
     def load(self, path):
         """Loads a stored conjunctive index."""
         if not os.path.isdir(path):
@@ -30,19 +33,35 @@ class ConjunctiveIndex:
         with open(full_path, 'w') as file:
             json.dump(self.index, file)
 
-    def put(self, word_id: int, document: str):
+    def put(self, word_id, document: str):
+        """Puts a document into the inverted index at the word_id.
+
+        :args
+        word_id - the word for which the document will be added.
+        document - the unique document name that will be added to the word in the index.
+        """
         try:
-            self.index[word_id].append(document)
+            self.index[word_id].put(document)
 
         except KeyError:
-            self.index[word_id] = [document]
+            documents = PriorityQueue()
+            documents.put(document)
+            self.index[word_id] = documents
 
     def get(self, word_id: int):
+        """Gets a list of documents that belong to the given word.
+
+        :args
+        word_id - the word for which the list of documents will be fetched.
+
+        :raises
+        a KeyError if the word does not exist in the index.
+        """
         try:
-            return self.index[word_id]
+            return self.index[word_id].queue
 
         except KeyError:
-            return []
+            raise KeyError(f'The word: {word_id}, does not exist in the index.')
 
     def query(self, query_words):
         pass
