cb35e2c8a560a2c50d9891ef7871a6d346a36ea6
jonasbarth <jb901@exeter.ac.uk>
2022-11-19 17:18:28 +0100

    Command Line * add comments explaining script * formatting

--- command_line.sh
+++ command_line.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+# A script for finding and printing the number of places, average number of visitors, and total number of people
+# that want to visit, for various countries. It isn't very efficient because the file is queried many times.
 
 countries=("Italy" "Spain" "France" "England" "United States")
 
 for i in ${!countries[@]}; do
-        # get number of places.
-        # 1. get the address column
-        # 2. grep rows that match the country
-        # 3. count the number of lines
 
         places=$(awk -F '\t' '{if (NR>1) printf ("%d\t%d\t%s\n", $4, $5, $10)}' places.tsv | grep "${countries[$i]}")
 
@@ -17,16 +15,15 @@ for i in ${!countries[@]}; do
                 echo "Number of people that want to go: 0"
                 continue
         fi
-                number_of_places=$(awk -F '\t' '{if (NR>1) printf ("%d\t%d\t%s\n", $5, $6, $10)}' places.tsv | grep "${countries[$i]}" | wc -l)
+
+        number_of_places=$(awk -F '\t' '{if (NR>1) printf ("%d\t%d\t%s\n", $5, $6, $10)}' places.tsv | grep "${countries[$i]}" | wc -l)
 
         # get total visitors.
         # 1. get the number of people went column (skip the first line)
         # 2. sum the values
-        # awk -F '\t' '{if (NR>1) printf ("%d\t%d\t%s\n", $4, $5, $9)}' places.tsv |
         total_visitors=$(awk -F '\t' '{if (NR>1) printf ("%d\t%d\t%s\n", $5, $6, $10)}' places.tsv | grep "${countries[$i]}" | awk '{s+=$1} END {print s}')
 
         # get average number of visitors
-        # 1. divide total visitors by number of places
         avg_visitors=$(($total_visitors / $number_of_places))
 
         # get number of people that want to go
